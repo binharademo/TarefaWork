a13b59bae3421e81e9aef2a8baf38d04325c745d
gbl3011 <gbl3011@gmail.com>
2025-04-07 10:08:03 -0300

    atualizar tarefa

---
+++ TarefasLib/ITarefaRepositorio.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Tarefas_Library
+{
+    public interface ITarefaRepositorio
+    {
+        Tarefa? BuscarPorID(int id);
+        List<Tarefa> ListarTodas();
+        bool Salvar(Tarefa tarefa);
+        public bool Atualizar(Tarefa tarefa, string novostatus, string novadescricao, DateTime novoprazo);
+    }
+}
---
+++ TarefasLib/TarefaServico.cs
@@ -0,0 +1,33 @@
+﻿namespace Tarefas_Library
+{
+    public class TarefaServico : ITarefaServico
+    {
+        ITarefaRepositorio _repositorio;
+
+        public TarefaServico(ITarefaRepositorio repositorio)
+        {
+            _repositorio = repositorio;
+        }
+
+        public bool Salvar(Tarefa tarefa)
+        {
+            return _repositorio.Salvar(tarefa);
+        }
+
+        public Tarefa BuscarPorId(int id)
+        {
+            return _repositorio.BuscarPorID(id);
+        }
+
+        public bool Atualizar(Tarefa tarefa, string novostatus, string novadescricao, DateTime novoprazo)
+        {
+            return _repositorio.Atualizar(tarefa, novostatus, novadescricao, novoprazo);    
+        }
+
+        public List<Tarefa> ListarTodas()
+        {
+            return _repositorio.ListarTodas();
+        }
+
+    }
+}
