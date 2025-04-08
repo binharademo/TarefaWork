98eb1a18bf79aae110b622b8a65090c465ca053d
gbl3011 <gbl3011@gmail.com>
2025-04-08 13:32:57 -0300

    cadastro de setor

---
+++ TarefasLib/Repositorio/SetorRepositorio.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TarefasLibrary.Interface;
+using TarefasLibrary.Modelo;
+
+namespace TarefasLibrary.Repositorio
+{
+    public class SetorRepositorio : IRepositorio<Setor>
+    {
+        private List<Setor> ListaSetores = new();
+        private int contadorIDs = 0;
+
+        public Setor? BuscarPorId(int id)
+        {
+            return ListaSetores.FirstOrDefault(s => s.Id == id);
+        }
+
+        public bool Cadastrar(Setor obj)
+        {
+            obj.Id = ++contadorIDs;
+            ListaSetores.Add(obj);
+            return true;
+        }
+
+        public bool Editar(Setor obj)
+        {
+            var setorExistente = BuscarPorId(obj.Id);
+            if (setorExistente != null)
+            {
+                setorExistente.Nome = obj.Nome;
+                setorExistente.Status = obj.Status;
+                return true;
+            }
+            return false;
+        }
+
+        public List<Setor> Listar()
+        {
+            return ListaSetores;
+        }
+
+        public bool Remover(Setor obj)
+        {
+            return ListaSetores.Remove(obj);
+        }
+    }
+
+}
+
---
+++ TestsTarefas/TesteSetores.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TarefasLibrary.Modelo;
+using TarefasLibrary.Negocio;
+using TarefasLibrary.Repositorio;
+
+namespace TestsTarefas
+{
+    public class TesteSetores
+    {
+        [Fact]
+        public void Cadastrar_Setor()
+        {
+            var repositorio = new SetorRepositorio();
+            var servico = new SetorServico(repositorio);
+
+            Setor setor = new Setor("Comercial");
+            var resultado = servico.Cadastrar(setor);
+
+            Assert.True(resultado);
+        }
+
+        [Fact]
+        public void CadastrarEListarSetor()
+        {
+            var repositorio = new SetorRepositorio();
+            var servico = new SetorServico(repositorio);
+
+            Setor setor = new Setor("Comercial");
+            var resultado = servico.Cadastrar(setor);
+
+            var listaSetores = servico.Listar();
+
+            Assert.True(resultado);
+            Assert.NotEmpty(listaSetores);
+            Assert.Contains(setor, listaSetores);
+        }
+
+        [Fact]
+        public void EditarSetor()
+        {
+            var repositorio = new SetorRepositorio();
+            var servico = new SetorServico(repositorio);
+
+            Setor setor = new Setor("Comercial");
+            servico.Cadastrar(setor);
+
+            var resultadoEdicao = servico.Editar(setor.Id, "Financeiro", true);
+
+            Assert.True(resultadoEdicao);
+
+        }
+
+    }
+}
