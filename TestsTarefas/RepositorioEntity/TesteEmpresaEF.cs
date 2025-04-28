d2d8a53667c397bf641183bbb6e62b3ceab14331
gbl3011 <gbl3011@gmail.com>
2025-04-28 09:30:09 -0300

    Persistencia Empresa Pronta

--- TestsTarefas/RepositorioEntity/TesteEmpresaEF.cs
+++ TestsTarefas/RepositorioEntity/TesteEmpresaEF.cs
@@ -28,5 +28,38 @@ namespace Tests_Tarefas.RepositorioEntity
             Assert.True(empresaBuscada);
 
         }
+
+        [Fact]
+        public void Editar_EmpresaEF()
+        {
+            var empresa = new Empresa("empresa vinicius", "12345678901234");
+            var empresaRepositorio = new EmpresaRepositorio(connectionString);
+            empresaRepositorio.InicializarBancoDados();
+            var empresaCadastrada = empresaRepositorio.Cadastrar(empresa);
+
+            // Act
+            empresa.Nome = "empresa editada";
+            var empresaEditada = empresaRepositorio.Editar(empresa);
+
+            // Assert
+            Assert.True(empresaEditada);
+        }
+        [Fact]
+        public void Deletar_EmpresaEF()
+        {
+            var empresa = new Empresa("empresa vinicius", "12345678901234");
+            var empresa2 = new Empresa("empresa nao excluida", "123");
+            var empresaRepositorio = new EmpresaRepositorio(connectionString);
+            empresaRepositorio.InicializarBancoDados();
+            var empresaCadastrada = empresaRepositorio.Cadastrar(empresa);
+            var empresaCadastrada2 = empresaRepositorio.Cadastrar(empresa2);
+
+            // Act
+            var empresaDeletada = empresaRepositorio.Remover(empresa);
+
+            // Assert
+            Assert.True(empresaDeletada);
+        }
+
     }
 }
