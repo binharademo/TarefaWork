6a918ec6e954611e5238b3f2146d718926938c38
Vinicius <desenvolvimento01@winaudio.com.br>
2025-04-28 12:59:23 -0300

    Criar tarefa ainda em execução

---
+++ BlazorTarefas/Program.cs
@@ -0,0 +1,44 @@
+using BlazorTarefas.Components;
+using TarefasLibrary.Interface;
+using TarefasLibrary.Negocio;
+using TarefasLibrary.Repositorio;
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+// Registrar serviços da aplicação
+builder.Services.AddSingleton<ITarefaRepositorio, TarefaMemoriaRepositorio>();
+builder.Services.AddSingleton<ITarefaServico, TarefaServico>();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseAntiforgery();
+app.UseSession();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
