a8f1dd1bfd4ffd3e2eb02ca4326f66b930ccdb92
gbl3011 <gbl3011@gmail.com>
2025-05-02 13:19:26 -0300

    Melhoria Teste Setor

--- TestsTarefas/RepositorioEntity/TesteSetorEF.cs
+++ TestsTarefas/RepositorioEntity/TesteSetorEF.cs
@@ -18,7 +18,7 @@ namespace Tests_Tarefas.RepositorioEntity
             // Arrange  
             var empresaRepositorio = new EmpresaRepositorio(connectionString);
             var empresa = empresaRepositorio.Listar().First();
-            
+
             if (empresa == null)
             {
                 empresa = new Empresa("empresa teste", "777777");
@@ -99,6 +99,106 @@ namespace Tests_Tarefas.RepositorioEntity
 
             // Assert
             Assert.True(resultadoExclusao);
+        }
+
+        [Fact]
+        public void Listar_setorEF()
+        {
+            // Arrange
+            var empresaRepositorio = new EmpresaRepositorio(connectionString);
+            var empresa = empresaRepositorio.Listar().First();
+            if (empresa == null)
+            {
+                empresa = new Empresa("empresa teste", "777777");
+                empresaRepositorio.Cadastrar(empresa);
+            }
+            var setorRepositorio = new SetorRepositorio(connectionString);
+            setorRepositorio.InicializarBancoDados();
+            var setor1 = new Setor("financeiro", empresa);
+            var setor2 = new Setor("ti", empresa);
+            setorRepositorio.Cadastrar(setor1);
+            setorRepositorio.Cadastrar(setor2);
+            // Act
+            var setores = setorRepositorio.Listar();
+            // Assert
+            Assert.NotEmpty(setores);
+            Assert.Contains(setores, s => s.Nome == "financeiro");
+            Assert.Contains(setores, s => s.Nome == "ti");
+        }
+
+        [Fact]
+        public void Excluir_setorNaoExistenteEF()
+        {
+            // Arrange
+            var empresaRepositorio = new EmpresaRepositorio(connectionString);
+            var empresa1 = empresaRepositorio.Listar().First();
+            if (empresa1 == null)
+            {
+                empresa1 = new Empresa("empresa1 teste", "777777");
+                empresaRepositorio.Cadastrar(empresa1);
+            }
+
+            var setorRepositorio = new SetorRepositorio(connectionString);
+            setorRepositorio.InicializarBancoDados();
+
+            var setor = new Setor("TesteSetorQueNaoExiste", empresa1);
+
+
+            // Act
+            var resultadoExclusao = setorRepositorio.Remover(setor);
+
+            // Assert
+            Assert.False(resultadoExclusao);
+        }
+
+        [Fact]
+        public void Editar_setorNaoExistenteEF()
+        {
+            // Arrange
+            var empresaRepositorio = new EmpresaRepositorio(connectionString);
+            var empresa1 = empresaRepositorio.Listar().First();
+            if (empresa1 == null)
+            {
+                empresa1 = new Empresa("empresa1 teste", "777777");
+                empresaRepositorio.Cadastrar(empresa1);
+            }
+
+            var setorRepositorio = new SetorRepositorio(connectionString);
+            setorRepositorio.InicializarBancoDados();
+
+            var setor = new Setor("TesteSetorQueNaoExiste", empresa1);
+
+
+            // Act
+            var resultadoEdicao = setorRepositorio.Editar(setor);
+
+            // Assert
+            Assert.False(resultadoEdicao);
+        }
+
+        [Fact]
+        public void ListarPorId_setorEF()
+        {
+            // Arrange
+            var empresaRepositorio = new EmpresaRepositorio(connectionString);
+            var setorRepositorio = new SetorRepositorio(connectionString);
+            var empresa = empresaRepositorio.Listar().First();
+
+            if (empresa == null)
+            {
+                empresa = new Empresa("empresa teste", "777777");
+                empresaRepositorio.Cadastrar(empresa);
+            }
+            var setor = setorRepositorio.Listar().FirstOrDefault(s => s.EmpresaId == empresa.Id);
+            if (setor == null)
+            {
+                setor = new Setor("financeiro", empresa);
+                setorRepositorio.Cadastrar(setor);
+            }
+            // Act
+            var setorBuscado = setorRepositorio.BuscarPorId(setor.Id);
+            // Assert
+            Assert.NotNull(setorBuscado);
         }
     }
 }
